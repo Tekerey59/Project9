428efb2453f464e7bcf4619c9527ab88c9f5aa72
Tekerey59 <sasbomb59@gmail.com>
2024-11-04 21:40:52 +0500

    bugfix + реализовна функция проверки повторного ввода пароля + реализована функция удаления вещества

--- app.py
+++ app.py
@@ -10,62 +10,62 @@ with sq.connect('base.db') as con:
 
                                                       #Обработка GET запросов
   @app.route('/')
-  def index():
+  def get_index():
     return render_template('index.html')
 
   @app.route('/account/')
-  def account():
+  def get_account():
     # COMING SOON
     return render_template('acc')
 
   @app.route('/account/edit/')
-  def account_edit_static():
+  def get_account_edit():
     # COMING SOON
     return render_template('acc_ed', err = False)
 
   @app.route('/account/delete/')
-  def account_delete():
+  def get_account_delete():
     # COMING SOON
     return render_template('')
 
   @app.route('/login/')
-  def login_static():
+  def get_login():
     # COMING SOON
     return render_template('')
 
   @app.route('/register/')
-  def register_static():
+  def get_register():
     # COMING SOON
     return render_template('register.html')
 
   @app.route('/<id>/')
-  def chem(id):
+  def get_chem(id):
     # COMING SOON
     return render_template('chem', id)
 
   @app.route('/<id>/edit/')
-  def chem_edit_static(id):
+  def get_chem_edit(id):
     # COMING SOON
     return render_template('chem_ed', id, err = False)
 
   @app.route('/search/q=&p=')
-  def search_res(id):
+  def get_search(id):
     # COMING SOON
     return render_template()
 
   @app.route('/like/')
-  def like(id):
+  def get_like(id):
     # COMING SOON
     return render_template()
 
   @app.route('/usful-information/')
-  def inf(id):
+  def get_inf(id):
     # COMING SOON
     return render_template()
 
                                                           #Обработка POST запросов
   @app.route('/account/edit/', methods=['POST'])
-  def account_edite_end():
+  def post_account_edite():
     # COMING SOON, For example
     t = True
     if t:
@@ -74,7 +74,7 @@ with sq.connect('base.db') as con:
       return render_template('acc_ed', err = True)
 
   @app.route('/account/delete', methods=['POST'])
-  def account_delete_conf():
+  def post_account_delete():
     passwd = True
     if passwd:
       return render_template('testpage.html')
@@ -82,37 +82,41 @@ with sq.connect('base.db') as con:
       return render_template('acc')
 
   @app.route('/<id>/edit/', methods=['POST'])
-  def chem_edite_end():
+  def post_chem_edite():
     # COMING SOON, For example
     e = True
     if e:
-      return render_template('chem', id)
+      return render_template('chem.html', id)
     else:
       return render_template('chem_ed', id, err = True)
 
   @app.route('/<id>/delete/', methods=['POST'])
-  def chem_delete_conf():
-    idt = True
+  def post_chem_delete():
+    idt = request.form['confirmation']
     if idt:
-      return render_template('index.html')
+      return render_template('index.html', notification = {"class":"success", "title":"Успех", "text":"Вещество удалено!"})
     else:
-      return render_template('chem', id)
+      return render_template('chem.html', notification = {"class":"error", "title":"Отмена", "text":"Вещество не было удалено"})
     
   @app.route('/register/', methods=['POST'])
-  def register(): #ГОТОВО, кроме 'register.html' --> ?
-    try:
-      name = request.form['name']
-      email = request.form['email']
-      password = request.form[password]
+  def post_register(): 
+    name = request.form['name']
+    email = request.form['email']
+    password_first = request.form['password_first']
+    password_second = request.form['password_second']
+    if password_first == password_second:
       created_datatime = time.ctime(time.time)
       updated_datatime = created_datatime
-      cur.execute(f"""INSERT INTO users (name, email, password, created_datetime, upgrated_datatime) VALUES ({name}, {email}, {password}, {created_datatime}, {updated_datatime})""")
-      return render_template('index.html', notification = {"class":"success", "title":"Успех", "text":"Регистрация выполнена!"})
-    except Exception as err:
-      return render_template('index.html', notification = {"class":"error", "title":"Ошибка", "text":"Ошибка регистрации, попробуйте ещё раз"})
+      try:
+        cur.execute(f"""INSERT INTO users (name, email, password, created_datetime, upgrated_datatime) VALUES ({name}, {email}, {password}, {created_datatime}, {updated_datatime})""")
+        return render_template('index.html', notification = {"class":"success", "title":"Успех", "text":"Регистрация выполнена!"})
+      except Exception as err:
+        return render_template('register.html', notification = {"class":"error", "title":"Ошибка", "text":"Ошибка регистрации, попробуйте ещё раз"})
+    else:
+      return render_template('register.html', notification = {"class":"error", "title":"Ошибка", "text":"Ошибка регистрации, пароли не совпадают, попробуйте ещё раз"})
     
   @app.route('/login/', methods=['POST'])
-  def login():
+  def post_login():
     name = request.form['name']
     password = request.form['password']
     cur.execute(f"""SELECT password FROM users WHERE name == {name}""")
